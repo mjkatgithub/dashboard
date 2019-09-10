bb9db70cd68d330fe78fccaa941af96542471d57
Melchior Kannengießer <melchior.kannengiesser@gmx.de>
2019-09-10 23:59:31 +0200

    Updates

---
+++ precache-manifest.ee68b0bb09e7e7ef2d4a915c23d06bbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cff0cf0a7c32bea810ad0fdcf946681",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "58affe85377bd19ea895",
+    "url": "/dashboard/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4a023363dc241b71634d",
+    "url": "/dashboard/static/js/2.0bf09cc4.chunk.js"
+  },
+  {
+    "revision": "58affe85377bd19ea895",
+    "url": "/dashboard/static/js/main.ef131969.chunk.js"
+  },
+  {
+    "revision": "f57fa5268be7b637514c",
+    "url": "/dashboard/static/js/runtime~main.a1705bb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dashboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
